d9d78a462a11c15b675e6ca59db87e002078e42d
Minh Ngoc <leminhngocodohai@gmail.com>
2020-10-06 15:48:14 +0700

    first commit

---
+++ app/Repositories/BaseRepository.php
@@ -0,0 +1,11 @@
+<?php
+namespace App\Repositories;
+
+use App\Repositories\ReponsitoryInterface;
+
+abstract class BaseRepository implements ReponsitoryInterface
+{
+    //
+}
+
+?>
---
+++ app/Repositories/User/UserRepository.php
@@ -0,0 +1,11 @@
+<?php
+namespace App\Repositories\User;
+
+use App\Repositories\BaseRepository;
+
+class UserRepository extends BaseRepository implements UserRepositoryInterface
+{
+
+}
+
+?>
